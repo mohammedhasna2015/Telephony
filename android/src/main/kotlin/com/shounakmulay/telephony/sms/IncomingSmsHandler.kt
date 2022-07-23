d3e1e710490f1c8ebaab032f2f943691c3f74d39
MacBook <macbook@dgl12s-MacBook-Pro.local>
2022-07-23 12:49:57 +0300

    improvement

--- android/src/main/kotlin/com/shounakmulay/telephony/sms/IncomingSmsHandler.kt
+++ android/src/main/kotlin/com/shounakmulay/telephony/sms/IncomingSmsHandler.kt
@@ -5,9 +5,13 @@ import android.app.KeyguardManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import android.os.Process
 import android.provider.Telephony
 import android.telephony.SmsMessage
+import android.telephony.SubscriptionManager
+import android.util.Log
+import android.widget.Toast
 import com.shounakmulay.telephony.utils.Constants
 import com.shounakmulay.telephony.utils.Constants.HANDLE
 import com.shounakmulay.telephony.utils.Constants.HANDLE_BACKGROUND_MESSAGE
@@ -41,14 +45,126 @@ class IncomingSmsReceiver : BroadcastReceiver() {
     companion object {
         var foregroundSmsChannel: MethodChannel? = null
     }
+    lateinit var subscriptionManager:SubscriptionManager
 
     override fun onReceive(context: Context, intent: Intent?) {
         ContextHolder.applicationContext = context.applicationContext
         val smsList = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+        subscriptionManager = SubscriptionManager.from(context.applicationContext)
+        val extras: Bundle = intent?.extras!!
+        if (extras != null) {
+            if (extras.containsKey("slot")){
+                Log.e("slot",extras.getInt("slot").toString() )
+            }else if (extras.containsKey("simId")){
+                Log.e("simId",extras.getInt("simId").toString() )
+            }else if (extras.containsKey("sub_id")){
+                Log.e("sub_id",extras.getInt("sub_id").toString() )
+            }else if (extras.containsKey("subscription")){
+                Log.e("subscription",extras.getInt("subscription").toString() )
+            }else if (extras.containsKey("com.android.phone.extra.slot")){
+                Log.e("com.android.phone.extra.slot",extras.getInt("com.android.phone.extra.slot").toString() )
+            }else if (extras.containsKey("sim")){
+                Log.e("sim",extras.getInt("sim").toString() )
+
+            }
+
+            Log.e("SIMDetection",detectSim(extras, context,subscriptionManager).toString())
+        }
         val messagesGroupedByOriginatingAddress = smsList.groupBy { it.originatingAddress }
         messagesGroupedByOriginatingAddress.forEach { group ->
             processIncomingSms(context, group.value)
         }
+    }
+
+    private fun detectSim(bundle: Bundle, context: Context, subscriptionManager: SubscriptionManager): String? {
+        var slot = -1
+        val keySet = bundle.keySet()
+        for (key in keySet) {
+            when (key) {
+                "phone" ->{
+                    slot = bundle.getInt("phone", -1)
+                    Log.e("keyname", key.toString())
+                }
+                "slot" ->
+                {slot = bundle.getInt("slot", -1)
+                    Log.e("keyname", key.toString())
+                }
+                "simId" ->
+                {slot = bundle.getInt("simId", -1)
+                    Log.e("keyname", key.toString())
+                }
+                "simSlot" ->
+                {slot = bundle.getInt("simSlot", -1)
+                    Log.e("keyname", key.toString())
+                }
+                "slot_id" ->
+                {slot = bundle.getInt("slot_id", -1)
+                    Log.e("keyname", key.toString())
+                }
+                "simnum" ->
+                {slot = bundle.getInt("simnum", -1)
+                    Log.e("keyname", key.toString())
+                }
+                "slotId" ->
+                {slot = bundle.getInt("slotId", -1)
+                    Log.e("keyname", key.toString())
+                }
+                "slotIdx" ->
+                {slot = bundle.getInt("slotIdx", -1)
+                    Log.e("keyname", key.toString())
+                }
+//                "subscription" -> slot = bundle.getInt("subscription", -1)
+                "com.android.phone.extra.slot" ->
+                {slot = bundle.getInt("com.android.phone.extra.slot", -1)
+                    Log.e("keyname", key.toString())
+                }
+                else -> if (key.lowercase(Locale.getDefault()).contains("slot") or key.lowercase(
+                        Locale.getDefault()
+                    ).contains("sim")
+                ) {
+                    val value = bundle.getString(key, "-1")
+                    if ((value == "0") or (value == "1") or (value == "2")) {
+                        slot = bundle.getInt(key, -1)
+                    }
+                }
+            }
+        }
+
+        Log.e("KKK", slot.toString())
+
+        if (bundle.containsKey("subscription")){
+            val subid = bundle.getInt("subscription",-1)
+            Log.e("GotButSubsribtionId",findSlotFromSubId(subscriptionManager,subid).toString())
+        }
+
+
+
+
+        return if (slot == 0) {
+            Toast.makeText(context, "sim1", Toast.LENGTH_SHORT).show()
+            "sim1"
+        } else if (slot == 1) {
+            Toast.makeText(context, "sim2", Toast.LENGTH_SHORT).show()
+            "sim2"
+        } else {
+            Toast.makeText(context, "undetected", Toast.LENGTH_SHORT).show()
+            "undetected"
+        }
+
+
+    }
+
+    fun findSlotFromSubId(sm: SubscriptionManager, subId: Int): Int {
+        try {
+            for (s in sm.activeSubscriptionInfoList) {
+                if (s.subscriptionId == subId) {
+                    return s.simSlotIndex
+                }
+            }
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+        return -1
     }
 
     /**
